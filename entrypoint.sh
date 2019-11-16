cea1cf1f243415b5bd179871081fb76a5a7ff6a5
Phoenix <navin25@hotmail.com>
2019-11-16 20:02:01 +0800

    Savepoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
 
-puts "hi"
+webhook_event_payload = ENV["GITHUB_EVENT_PATH"]
+
+# # github_client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+
+# # pull_request = github_client
+
+puts webhook_event_payload
+

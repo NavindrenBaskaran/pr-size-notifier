daf43dac2d707da177904df0079057fd87a5b5a5
Phoenix <navin25@hotmail.com>
2019-11-16 19:06:06 +0800

    Update: action to print hi

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 # require 'octokit'
 
-webhook_event_payload = ENV["GITHUB_EVENT_PATH"]
+# webhook_event_payload = ENV["GITHUB_EVENT_PATH"]
 
 # github_client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 
 # pull_request = github_client
 
-puts webhook_event_payload
+puts "Hi from ruby"
 

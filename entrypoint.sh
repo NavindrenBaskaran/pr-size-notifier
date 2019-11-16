1dd962cda0debb203ca16d65a9afe4e6593f11b4
Phoenix <navin25@hotmail.com>
2019-11-16 23:44:12 +0800

    savepoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ require 'octokit'
 
 webhook_event_payload = File.read(ENV["GITHUB_EVENT_PATH"])
 webhook_event_payload_in_json = JSON.parse(webhook_event_payload)
-repo_name = webhook_event_payload_in_json["repo"]["full_name"]
+repo_name = webhook_event_payload_in_json["head"]["repo"]["full_name"]
+
 acceptable_pr_size = ENV["ACCEPTABLE_PR_SIZE"] || 10
 
 github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
@@ -27,9 +28,9 @@ if current_pull_request
   puts "pr_number: #{pr_number}"
   puts "pr: #{pr}"
 
-  # total_addition_and_deletions = pr["additions"] + pr["deletions"]
+  total_addition_and_deletions = pr["additions"] + pr["deletions"]
 
-  # if total_addition_and_deletions > acceptable_pr_size
-  #   github.add_comment(repo_name, pr_number, "This pull request is big. We prefer smaller PRs whenever possible, as they are easier to review. Can this be split into a few smaller PRs?")
-  # end
+  if total_addition_and_deletions > acceptable_pr_size
+    github.add_comment(repo_name, pr_number, "This pull request is big. We prefer smaller PRs whenever possible, as they are easier to review. Can this be split into a few smaller PRs?")
+  end
 end

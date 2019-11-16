50facb9b01a3c8655d80d58b8316a34e171e6e93
Phoenix <navin25@hotmail.com>
2019-11-16 23:05:05 +0800

    savepoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ opened_pull_requests = github.pull_requests("NavindrenBaskaran/sign_up", state: 
 
 current_pull_request = opened_pull_requests.select { |opened_pull_request| opened_pull_request["head"]["sha"] == most_recent_commit_hash.to_s }.last
 
-puts "current_pull_request #{current_pull_request}"
-
-pr_number = current_pull_request["number"]
-github.add_comment("NavindrenBaskaran/sign_up", pr_number, "Done!")
+if current_pull_request
+  pr_number = current_pull_request["number"]
+  github.add_comment("NavindrenBaskaran/sign_up", pr_number, "Done!")
+end

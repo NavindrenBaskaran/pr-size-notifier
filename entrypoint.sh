9b98950fd930676e7aa0d8652901f280d6c087fe
Phoenix <navin25@hotmail.com>
2019-11-16 23:38:06 +0800

    savepoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,9 +22,14 @@ current_pull_request = opened_pull_requests.select { |opened_pull_request| opene
 if current_pull_request
   pr_number = current_pull_request["number"]
   pr = github.pull_request(repo_name, pr_number, state: 'open')
-  total_addition_and_deletions = pr["additions"] + pr["deletions"]
 
-  if total_addition_and_deletions > acceptable_pr_size
-    github.add_comment(repo_name, pr_number, "This pull request is big. We prefer smaller PRs whenever possible, as they are easier to review. Can this be split into a few smaller PRs?")
-  end
+  puts "repo name: #{repo_name}"
+  puts "pr_number: #{pr_number}"
+  puts "pr: #{pr}"
+
+  # total_addition_and_deletions = pr["additions"] + pr["deletions"]
+
+  # if total_addition_and_deletions > acceptable_pr_size
+  #   github.add_comment(repo_name, pr_number, "This pull request is big. We prefer smaller PRs whenever possible, as they are easier to review. Can this be split into a few smaller PRs?")
+  # end
 end

5fb4f5e45b0d3583e693d18ec9be87ca52fac378
Phoenix <navin25@hotmail.com>
2019-11-16 21:15:47 +0800

    savepoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ require 'octokit'
 webhook_event_payload = File.read(ENV["GITHUB_EVENT_PATH"])
 webhook_event_payload_in_json = JSON.parse(webhook_event_payload)
 
-github = Octokit::Client.new(access_token: "8f821cbf81d2a1fed9160cd723b5c2206a262361")
+github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 
 most_recent_commit_hash = webhook_event_payload_in_json["after"]
 

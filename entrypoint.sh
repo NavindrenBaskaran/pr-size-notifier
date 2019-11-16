db365182d2a7372aa07f8f6e85eab40da48c2402
Phoenix <navin25@hotmail.com>
2019-11-16 20:32:24 +0800

    Add: capability to comment on a PR

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env ruby
 
+require 'octokit'
+
 webhook_event_payload = File.read(ENV["GITHUB_EVENT_PATH"])
 
-# # github_client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+github_client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 
-# # pull_request = github_client
+most_recent_commit_hash = webhook_event_payload["after"]
 
-puts webhook_event_payload
+opened_pull_requests = github_client.pull_requests("NavindrenBaskaran/sign_up", state: 'closed')
+
+current_pull_request = opened_pull_requests.select { |opened_pull_request| opened_pull_request["head"]["sha"] == most_recent_commit_hash }.last
+
+if current_pull_request.present?
+  pr_number = current_pull_request["number"]
+  github_client.add_comment("NavindrenBaskaran/sign_up", pr_number, "Done!")
+end
 
